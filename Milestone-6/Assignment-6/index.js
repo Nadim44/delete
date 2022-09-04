d54ddafdf4eb8423de1ce07f2944befdfedb4e5b
2Nadim <nadimmahamud661@gmail.com>
2022-09-04 06:07:10 +0100

    recheck and final push

--- Milestone-6/Assignment-6/index.js
+++ Milestone-6/Assignment-6/index.js
@@ -25,7 +25,6 @@ const newsFeeds = async (categoryId) => {
     const url = `https://openapi.programming-hero.com/api/news/category/${categoryId}`;
     const res = await fetch(url);
     const data = await res.json();
-
     displayNews(data.data)
 }
 
