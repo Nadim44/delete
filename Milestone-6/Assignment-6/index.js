f08f6294d5433a7c558fba319a2f12a1be2848ba
2Nadim <nadimmahamud661@gmail.com>
2022-09-04 05:12:38 +0100

    changed loop option

--- Milestone-6/Assignment-6/index.js
+++ Milestone-6/Assignment-6/index.js
@@ -1,69 +1,3 @@
-// const displayNewHeading = headings => {
-//     // console.log(headings)
-//     const newsContainer = document.getElementById('news-container')
-//     newsContainer.innerHTML = ''
-//     for (const heading of headings) {
-//         const newsTitle = document.createElement('a')
-//         newsTitle.innerHTML = `
-//         <a onclick="newsFeeds('${category_id}')" class="nav-link">${heading.category_name}</a>
-//         `;
-//         newsContainer.append(newsTitle)
-
-//     }
-
-// }
-// const loadNewsHeading = () => {
-//     // console.log('add')
-//     fetch('https://openapi.programming-hero.com/api/news/categories')
-//         .then(res => res.json())
-//         .then(data => displayNewHeading(data.data.news_category))
-
-// }
-
-// const newsFeeds = async (category_id) => {
-//     // const url = `https://openapi.programming-hero.com/api/news/category/01`;
-//     const url = `https://openapi.programming-hero.com/api/news/category/${category_id}`;
-//     const res = await fetch(url);
-//     const data = await res.json();
-//     displayNews(data.data)
-// }
-
-// const displayNews = (allNews) => {
-//     // console.log(allNews)
-//     const newsPartContainer = document.getElementById('news-part')
-//     // newsPart.createElement = ('div')
-//     newsPartContainer.innerHTML = ''
-//     for (const news of allNews) {
-//         console.log(news)
-//         // const newsPartContainer = document.getElementById('news-part')
-//         // // newsPart.createElement = ('div')
-//         // newsPartContainer.innerHTML = ''
-//         const newsPart = document.createElement('div')
-//         newsPart.innerHTML = `
-//         <div class="d-flex border border-dark ">
-//                 <div class="card">
-//                     <img src="${news.thumbnail_url}" alt="...">
-//                 </div>
-//                 <div class="card-body">
-//                     <h5 class="card-title">${news.title}</h5>
-//                     <h5 class="card-title">${news.total_view}</h5>
-//                     <p class="card-text">This is a longer card with supporting text below as a natural lead-in
-//                         to additional content. This content is a little bit longer.</p>
-//                 </div>
-
-//             </div>
-
-//         `;
-//         newsPartContainer.appendChild(newsPart)
-//     }
-
-
-// }
-
-
-
-
-
 
 const loadNewsHeading = () => {
     // console.log('add')
@@ -76,20 +10,17 @@ const displayNewHeading = headings => {
     // console.log(headings)
     const newsContainer = document.getElementById('news-container')
     newsContainer.innerHTML = ''
-    for (const heading of headings) {
+    // for (const heading of headings) {
+    headings.forEach(heading => {
         const newsTitle = document.createElement('a')
         newsTitle.innerHTML = `
         <a onclick="newsFeeds('${heading.category_id}')" class="nav-link">${heading.category_name}</a>
         `;
         newsContainer.append(newsTitle)
 
-
-    }
-
-
-}
+    });
+};
 const newsFeeds = async (categoryId) => {
-
     // const url = `https://openapi.programming-hero.com/api/news/category/01`;
     const url = `https://openapi.programming-hero.com/api/news/category/${categoryId}`;
     const res = await fetch(url);
@@ -100,12 +31,11 @@ const newsFeeds = async (categoryId) => {
 
 const displayNews = (allNews) => {
     // console.log(allNews)
-
-
     const newsPartContainer = document.getElementById('news-part')
     // toggleSpinner(true)
-    newsPartContainer.innerHTML = ''
-    for (const news of allNews) {
+    newsPartContainer.innerHTML = '';
+    // for (const news of allNews) {
+    allNews.forEach(news => {
         // console.log(news)
         const newsPart = document.createElement('div')
         newsPart.innerHTML = `
@@ -131,15 +61,10 @@ const displayNews = (allNews) => {
                        </div> 
                     </div> 
                 </div>
-
-
             </div>
-
-
         `;
         newsPartContainer.appendChild(newsPart)
-    }
-
+    });
 
 }
 
@@ -150,36 +75,6 @@ const toggleSpinner = isLoading => {
         loaderSection.classList.remove('d-none')
     }
 }
-
-
-// const details = () => {
-//     // console.log('add')
-//     const url = `https://openapi.programming-hero.com/api/news/0282e0e58a5c404fbd15261f11c2ab6a`
-//     fetch(url)
-//         .then(res => res.json())
-//         .then(data => displayDetails(data.data))
-
-// }
-// const displayDetails = (details) => {
-
-//     const modal=document.createElement('div')
-
-// }
-// details()
-
-
-// newsFeeds()
-
-
-// const newsFeeds = (category_id) => {
-
-//     const url = `https://openapi.programming-hero.com/api/news/category/{category_id}`;
-//     // const url = `https://openapi.programming-hero.com/api/news/category/01`;
-//     fetch(url)
-//         .then(res => res.json())
-//         .then(data => displayNews(data.data))
-// }
-
 
 
 const questionBlog = () => {
@@ -204,13 +99,15 @@ const questionBlog = () => {
     </div>
     
     `
-    questionBlog.appendChild(blog)
-}
+    questionBlog.appendChild(blog);
+};
+
+
 
 
 const newsDetailsModal = async (id) => {
-    const url = `https://openapi.programming-hero.com/api/news/${id}`
-    // const url = `https://openapi.programming-hero.com/api/news/0282e0e58a5c404fbd15261f11c2ab6a`
+    // const url = `https://openapi.programming-hero.com/api/news/${id}`
+    const url = `https://openapi.programming-hero.com/api/news/0282e0e58a5c404fbd15261f11c2ab6a`
     const res = await fetch(url)
     const data = await res.json();
     displayDetailsModal(data.data)
